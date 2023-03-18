63130389803da6dafc4d1215baec4f3eb30d7772
Mohamed <mohamed.amin@callvita-eg.com>
2023-03-18 03:03:41 +0200

    formatting

--- cruds/crud.js
+++ cruds/crud.js
@@ -3,7 +3,7 @@ var productPrice = document.getElementById("price");
 var productImg = document.getElementById("img");
 var productDesc = document.getElementById("desc");
 var submitBtn = document.getElementById("addBtn");
-var submitEditBtn = document.getElementById("editSubmit"); 
+var submitEditBtn = document.getElementById("editSubmit");
 submitBtn.addEventListener("click", addProduct);
 submitEditBtn.addEventListener("click", submitEditProduct);
 
@@ -14,30 +14,35 @@ if (localStorage.getItem("productsList") != null) {
 }
 
 function addProduct() {
-if (
-  !productName.value ||
-  !productPrice.value ||
-  !productImg.value ||
-  !productDesc.value
-) {
-  document.getElementById("alert").classList.remove("hide");
-  document.getElementById("alert").classList.add("show");
-}else{
-  document.getElementById("alert").classList.add("hide");
-  document.getElementById("alert").classList.remove("show");
-  var img = productImg.files[0];
-  var ImgSrc = `${URL.createObjectURL(img)}`;
-  var product = {
-    name: productName.value,
-    price: productPrice.value,
-    desc: productDesc.value,
-    img: ImgSrc,
-  };
-  productsList.push(product);
-  localStorage.setItem("productsList", JSON.stringify(productsList));
-  display(productsList);
-}
-clear()
+  if (
+    !productName.value ||
+    !productPrice.value ||
+    !productImg.value ||
+    !productDesc.value
+  ) {
+    document.getElementById("alert").classList.remove("hide");
+    document.getElementById("alert").classList.add("show");
+  } else {
+    document.getElementById("alert").classList.add("hide");
+    document.getElementById("alert").classList.remove("show");
+    var img = productImg.files[0];
+    var ImgSrc = `${URL.createObjectURL(img)}`;
+    var product = {
+      name: productName.value,
+      price: productPrice.value,
+      desc: productDesc.value,
+      img: ImgSrc,
+    };
+    productsList.push(product);
+    localStorage.setItem("productsList", JSON.stringify(productsList));
+    display(productsList);
+    window.scrollTo({
+      left: 0,
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+    clear();
+  }
 }
 
 function display(List) {
@@ -46,13 +51,13 @@ function display(List) {
   if (List.length == 0) {
     productsContainer.innerHTML = `<img src="./notfound.png" style="height: 400px;width:400px;margin:auto"></img>`;
   } else {
-  var data = "";
-  for (let i = 0; i < List.length; i++) {
-    data += `
+    var data = "";
+    for (let i = 0; i < List.length; i++) {
+      data += `
       <div class="productContainer">
         <div class="product">
           <div class="productBody">
-            <p>${List[i].name}</p>
+            <h3>${List[i].name}</h3>
             <p>
               ${List[i].price}
               <span style="color: blue"> EGP</span>
@@ -62,14 +67,14 @@ function display(List) {
           <div class="productDesc">
             <p>${List[i].desc}</p>
             <button id=deleteBtn onClick="deleteProduct(${i})">delete </button>
-            <a href="#form" ><button id=editBtn onClick="editProduct(${i})">edit </button></a>
+            <button id=editBtn onClick="editProduct(${i})">edit </button>
           </div>
         </div>
       </div>
   `;
+    }
+    productsContainer.innerHTML = data;
   }
-  productsContainer.innerHTML = data;
-}
 }
 
 function deleteProduct(index) {
@@ -78,22 +83,28 @@ function deleteProduct(index) {
   display(productsList);
 }
 
-var Pindex ;
+var Pindex;
 function editProduct(index) {
-Pindex = index;
-submitBtn.classList.add("hide")
-submitEditBtn.classList.replace("hide","show");
-productName.value = productsList[index].name;
-productPrice.value = productsList[index].price;
-productDesc.value = productsList[index].desc;
+  Pindex = index;
+  submitBtn.classList.add("hide");
+  submitEditBtn.classList.replace("hide", "show");
+  productName.value = productsList[index].name;
+  productPrice.value = productsList[index].price;
+  productDesc.value = productsList[index].desc;
+  display(productsList);
+  window.scrollTo({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
 }
 
 function submitEditProduct() {
-var NewName = productName.value;
-var NewPrice = productPrice.value;
-var newDesc = productDesc.value;
-var newImg = productImg.files[0];
-var newImgSrc = `${URL.createObjectURL(newImg)}`;
+  var NewName = productName.value;
+  var NewPrice = productPrice.value;
+  var newDesc = productDesc.value;
+  var newImg = productImg.files[0];
+  var newImgSrc = `${URL.createObjectURL(newImg)}`;
   productsList = JSON.parse(localStorage.getItem("productsList"));
   productsList[Pindex] = {
     name: NewName,
@@ -102,22 +113,25 @@ var newImgSrc = `${URL.createObjectURL(newImg)}`;
     img: newImgSrc,
   };
   display(productsList);
-
   localStorage.setItem("productsList", JSON.stringify(productsList));
+  window.scrollTo({
+    left: 0,
+    top: document.body.scrollHeight,
+    behavior: "smooth",
+  });
   submitBtn.classList.remove("hide");
   submitEditBtn.classList.replace("show", "hide");
-  clear()
+  clear();
 }
-
 
 function search() {
   let search = document.getElementById("search").value;
   var searched = [];
-  for (var i = 0; i < productsList.length; i++){
+  for (var i = 0; i < productsList.length; i++) {
     if (productsList[i].name.toUpperCase().includes(search.toUpperCase())) {
       searched.push(productsList[i]);
       display(searched);
-    }else{
+    } else {
       display([]);
     }
   }
